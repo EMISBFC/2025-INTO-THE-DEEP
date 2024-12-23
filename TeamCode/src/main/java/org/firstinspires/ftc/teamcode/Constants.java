7c913412d1f2a9215b6391214ec135a4241b63e6
ofek256 <ofek256a@gmail.com>
2024-12-23 19:30:24 +0200

    added shitty arm PID (needs tuning!)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode;
-
 public class Constants {
     public static double armP = 2.5, armI = 0.75, armD= 0.5, armF = 0.225;
     public static final double LOGRIPPER_OPEN_POS = 0.9;
